ecb1c95a50cd371c6f5c3c35b8077c30574dcf02
Швейкин Михаил Сергеевич <m.shveykin@tinkoff.ru>
2022-12-04 19:52:50 +0300

    0_basics: fix output

--- src/cli/env.js
+++ src/cli/env.js
@@ -4,11 +4,12 @@
 const parseEnv = () => {
   // Write your code here
 
-  for (const [key, value] of Object.entries(process.env)) {
-    if (key.startsWith("RSS_")) {
-      console.log(`${key}=${value};`);
-    }
-  }
+  console.log(
+    Object.entries(process.env)
+      .filter(([key]) => key.startsWith("RSS_"))
+      .map(([key, value]) => `${key}=${value}`)
+      .join("; ")
+  );
 };
 
 parseEnv();
