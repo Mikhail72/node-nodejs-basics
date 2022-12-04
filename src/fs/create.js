a45f7e8de0922a45e3ee5504f21422c11e2ab5fc
Швейкин Михаил Сергеевич <m.shveykin@tinkoff.ru>
2022-12-04 20:03:58 +0300

    0_basics: fix create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,21 +1,23 @@
-import fs from 'node:fs/promises';
-import path from 'node:path';
+import fs from "node:fs/promises";
+import path from "node:path";
 
 //create.js - implement function that creates new file fresh.txt
 // with content I am fresh and young inside of the files folder
 // (if file already exists Error with message FS operation failed must be thrown)
 
 const create = async () => {
-   // Write your code here 
-  const dir = path.resolve(process.cwd(), 'src', 'fs', 'files');
+  // Write your code here
+  const src = path.resolve(process.cwd(), "src", "fs", "files", "fresh.txt");
 
   try {
-    await fs.access(dir);
-    await fs.writeFile(path.resolve(`${dir}`, 'fresh.txt'), 'I am fresh and young inside', 'utf-8');
+    await fs.writeFile(src, "I am fresh and young inside", {
+      encoding: "utf-8",
+      flag: "wx",
+    });
   } catch (error) {
-    throw new Error('FS operation failed');
+    console.log(error);
+    throw new Error("FS operation failed");
   }
-
 };
 
 await create();
